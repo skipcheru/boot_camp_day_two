e7eb0a5fd7acb70961307a1bb1e8b61edea81468
Sammy Cheruiyot <skipcheru@gmail.com>
2016-11-03 11:13:29 +0300

    add proper variable names and comment notations

--- word_count.py
+++ word_count.py
@@ -1,23 +1,22 @@
+#  Function counts the number times a word occurs in a sentence
 
-"""Function counts the occurence of words in a string"""
+def words(sentence):
 
-def words(word):
-
-    split_words = str(word).split()
+    split_words = str(sentence).split()
     word_dict = dict()
 
-    """loop through the list and check each if it exists"""
+    # loop through the list and check if it exists
 
-    for sw in split_words:
-        if sw.isdigit() is True:
+    for word in split_words:
+        if word.isdigit() is True:
 
-            sw = int(sw)  # cast to integer if is a digit
+            word = int(word)  # cast to integer if is a digit
 
-        """add values as keys to the dictionary while checking if they exist"""
+        # add values as keys to the dictionary while checking if they exist
 
-        if sw not in word_dict:
-            word_dict[sw] = 1 # value is one if it doesnt exist
+        if word not in word_dict:
+            word_dict[word] = 1 # value is one if it doesnt exist
         else:
-            word_dict[sw] += 1 #increment value by one if they exist
+            word_dict[word] += 1 # increment value by one if they exist
 
     return word_dict
